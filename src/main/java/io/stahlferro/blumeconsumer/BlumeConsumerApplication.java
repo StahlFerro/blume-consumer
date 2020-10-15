63bff43a2b70a497e0b747c4c7643558498c8bf3
StahlFerro <StahlFerro@users.noreply.github.com>
2020-10-15 11:46:43 +0700

    First commit

---
+++ src/main/java/io/stahlferro/blumeconsumer/BlumeConsumerApplication.java
@@ -0,0 +1,13 @@
+package io.stahlferro.blumeconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlumeConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlumeConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/stahlferro/blumeconsumer/BlumeConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.stahlferro.blumeconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlumeConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
